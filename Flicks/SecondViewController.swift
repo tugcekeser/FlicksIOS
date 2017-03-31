598393859a1d789c28592a9433e6baca337c73d8
tugcekeser <tgcksr@gmail.com>
2017-03-31 09:52:49 -0700

    Initial Commit

---
+++ Flicks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Flicks
+//
+//  Created by Tuze on 3/31/17.
+//  Copyright © 2017 Tugce Keser. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
