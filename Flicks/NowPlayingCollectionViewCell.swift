03da55631ba4ec136bd70b052063173f8f45d580
tugcekeser <tgcksr@gmail.com>
2017-03-31 17:20:52 -0700

    First Commit

---
+++ Flicks/NowPlayingCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NowPlayingCollectionViewCell.swift
+//  Flicks
+//
+//  Created by Tuze on 3/31/17.
+//  Copyright © 2017 Tugce Keser. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingCollectionViewCell: UICollectionViewCell {
+    
+}
